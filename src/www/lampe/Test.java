7523629587e1035ada14053a69a19c86fd7f6577
StraetenMax <maxstraeten@yahoo.com>
2017-03-25 18:39:55 +0100

    mise en place du systeme

--- src/www/lampe/Test.java
+++ src/www/lampe/Test.java
@@ -3,8 +3,14 @@ package www.lampe;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Lampe lampe = new Lampe(true);
+			System.out.println(lampe.estAllumee());//true
+			lampe.presserInterrupteur();
+			System.out.println(lampe.estAllumee());//false
+			
+			System.out.println(lampe.estAllumee());
+			lampe.presserInterrupteur();
+			System.out.println(lampe.estAllumee());
 	}
 
 }
