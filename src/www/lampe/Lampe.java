27b83f6b44412380b6762b00b859d3309bb91fea
StraetenMax <maxstraeten@yahoo.com>
2017-03-25 18:08:26 +0100

    mise en place de l'attribut, du constructeur et d'une fonction

--- src/www/lampe/Lampe.java
+++ src/www/lampe/Lampe.java
@@ -1,5 +1,20 @@
 package www.lampe;
 
 public class Lampe {
-
+	
+	private boolean estAllumee;
+	
+	public Lampe(boolean etat){
+		estAllumee = etat;
+	}
+	public void presserInterrupteur(){
+		if(estAllumee == true){
+			estAllumee = false;
+		}else{
+			estAllumee =true;
+		}
+		
+	}
+	
+	
 }
